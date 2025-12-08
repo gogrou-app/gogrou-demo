5dc331369add55e33ca98d8d921ea367434ed9f2
gogrou-app <gogrou@gogrou.com>
2025-12-08 18:04:25 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,8 @@
-
+export default function Page() {
+  return (
+    <div style={{ padding: 40, fontSize: 24 }}>
+      <h1>GOGROU DEMO</h1>
+      <p>App router běží správně 🎉</p>
+    </div>
+  );
+}
