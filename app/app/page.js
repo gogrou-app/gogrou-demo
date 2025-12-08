847db5249ca4029bbaa0792b889ba650470155a1
gogrou-app <gogrou@gogrou.com>
2025-12-08 18:00:54 +0100

    Delete app/app directory

--- app/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
-      <h1>GOGROU DEMO</h1>
-      <p>Toto je základní verze – GPC + GSS + AI preview.</p>
-    </div>
-  );
-}
