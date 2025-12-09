5a67a0bf7e022fb80684fd40f937264fb13c2f4c
gogrou-app <gogrou@gogrou.com>
2025-12-09 16:39:57 +0100

    Update data.js

--- app/gpc/data.js
+++ app/gpc/data.js
@@ -1,35 +1,27 @@
 export const tools = [
   {
     id: 1,
+    gpc_id: "GPC-000001",
     name: "860.1-1050-056A1-MM",
     name_display: "Sandvik Coromant 860.1-1050-056A1-MM",
-    type: "Vrták",
     manufacturer: "Sandvik Coromant",
-    gtin: "–",
+    order_id: "8419421",
+    type: "Vrták",
     diameter: "10.5 mm",
+
+    // hlavní obrázek produktu
     image: "https://productinformation.sandvik.coromant.com/s3/documents/pictures/pict-3d-view/ext-preview/44372813__400X40072Dpi.png",
-    description: "Vysoce výkonný monolitní vrták Sandvik 860.1 pro přesné vrtání otvorů.",
-  },
-  {
-    id: 2,
-    name: "860.1-1050-056A1-MM",
-    name_display: "Sandvik Coromant 860.1-1050-056A1-MM – varianta B",
-    type: "Vrták",
-    manufacturer: "Sandvik Coromant",
-    gtin: "–",
-    diameter: "10.5 mm",
-    image: "https://common-secoresources.azureedge.net/pictures/core/Content/ProductImages/As_Delivered_Image/2017-04-10-140905_20175009.jpg?v=2017-04-10%2014:09:05Z",
-    description: "Alternativní varianta vrtáku z PDF datasheetu.",
-  },
-  {
-    id: 3,
-    name: "SD205A-1050-056-12R1-P",
-    name_display: "SD205A-1050-056-12R1-P",
-    type: "Vrták",
-    manufacturer: "Seco",
-    gtin: "–",
-    diameter: "10.5 mm",
-    image: "https://common-secoresources.azureedge.net/pictures/core/Content/ProductImages/As_Delivered_Image/2017-04-10-140905_20175009.jpg?v=2017-04-10%2014:09:05Z",
-    description: "Seco vrták SD205A pro hluboké vrtání s chladicími kanály.",
+
+    description: "Vysoce výkonný monolitní vrták Sandvik 860.1 pro přesné obrábění otvorů.",
+
+    // detailní parametry – zatím prázdné, doplníme v další fázi
+    details: {
+      technical_drawing: null,
+      geometry: {},
+      cooling: null,
+      material: null,
+      tolerances: {},
+      standard: null
+    }
   }
 ];
