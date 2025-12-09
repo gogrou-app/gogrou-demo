fa9578b7884f62928ae155d00d55a66f16b2cd11
gogrou-app <gogrou@gogrou.com>
2025-12-09 09:41:16 +0100

    Create page.js

---
+++ app/ss/page.js
@@ -0,0 +1,15 @@
+export default function Page() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>SmartSplit – Dynamický rozdělovací modul</h1>
+      <p>Zde bude mini-demo SmartSplit:</p>
+
+      <ul style={{ marginTop: "20px", fontSize: "18px", lineHeight: "1.6" }}>
+        <li>Společné nákupy</li>
+        <li>Rozdělování cen a množství</li>
+        <li>Vyhodnocení akce</li>
+        <li>Přehled účastníků</li>
+      </ul>
+    </div>
+  );
+}
