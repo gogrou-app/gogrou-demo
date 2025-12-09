5f53d78388f45a09e492d999d86c226337bb3267
gogrou-app <gogrou@gogrou.com>
2025-12-09 15:34:08 +0100

    Create data.js

---
+++ app/gpc/data.js
@@ -0,0 +1,14 @@
+export const tools = [
+  {
+    id: "t01",
+    name: "Vrták monolitní karbidový",
+    gtin: "1234567890123",
+    manufacturer: "MTTM",
+    diameter: "6.0 mm",
+    length: "57 mm",
+    coating: "TiAlN",
+    type: "Vrtání",
+    image: "/placeholder-tool.png",
+    description: "Vysoce výkonný monolitní karbidový vrták vhodný pro oceli 42–55 HRC.",
+  },
+];
