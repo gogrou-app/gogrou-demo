95dfbf990afc49e711351fb62874c01e602e63c9
gogrou-app <gogrou@gogrou.com>
2025-12-09 16:13:22 +0100

    Update data.js

--- app/gpc/data.js
+++ app/gpc/data.js
@@ -1,14 +1,37 @@
 export const tools = [
   {
-    id: "t01",
+    id: 1,
     name: "Vrták monolitní karbidový",
     gtin: "1234567890123",
     manufacturer: "MTTM",
     diameter: "6.0 mm",
     length: "57 mm",
     coating: "TiAlN",
-    type: "Vrtání",
-    image: "/placeholder-tool.png",
-    description: "Vysoce výkonný monolitní karbidový vrták vhodný pro oceli 42–55 HRC.",
+    description:
+      "Vysoce výkonný monolitní karbidový vrták vhodný pro oceli 42–55 HRC."
   },
+
+  {
+    id: 2,
+    name: "Fréza čtyřbřitá karbidová – univerzální",
+    gtin: "9876543210001",
+    manufacturer: "MTTM",
+    diameter: "10.0 mm",
+    length: "75 mm",
+    coating: "ALTICROME",
+    description:
+      "Univerzální karbidová fréza pro ocel, nerez i litinu. Vyvážený výkon, ideální pro menší a střední série."
+  },
+
+  {
+    id: 3,
+    name: "VBD – destička pro soustružení ISO DCMT 11T304",
+    gtin: "5200123400011",
+    manufacturer: "CERATIZIT",
+    diameter: "N/A",
+    length: "N/A",
+    coating: "CVD TiCN + Al2O3",
+    description:
+      "Standardní karbidová destička pro dokončovací soustružení ocelí. Vysoká životnost díky CVD povlaku."
+  }
 ];
