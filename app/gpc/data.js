6a78133c660171f6924b6dda0e2142f071aba55f
gogrou-app <gogrou@gogrou.com>
2025-12-09 16:35:10 +0100

    Update data.js

--- app/gpc/data.js
+++ app/gpc/data.js
@@ -1,53 +1,35 @@
 export const tools = [
   {
-    id: "mttm-drill-6mm",
-    name: "Vrták monolitní karbidový",
-    manufacturer: "MTTM",
-    gtin: "1234567890123",
-    image: "/placeholder.png",
-    description:
-      "Vysoce výkonný monolitní karbidový vrták vhodný pro oceli 42–55 HRC.",
-    parameters: {
-      diameter: "6.0 mm",
-      length: "57 mm",
-      coating: "TiAIN",
-      shank: "6 mm",
-    },
-  },
-
-  // -------------------------------------------------------------------
-  // SANDVIK – CoroDrill 860-MM (DODANÁ DATA)
-  // -------------------------------------------------------------------
-  {
-    id: "sandvik-860-1050-056A1",
-    name: "CoroDrill 860-MM – monolitní karbidový vrták",
+    id: 1,
+    name: "860.1-1050-056A1-MM",
+    name_display: "Sandvik Coromant 860.1-1050-056A1-MM",
+    type: "Vrták",
     manufacturer: "Sandvik Coromant",
-    gtin: "8419421",
-    image:
-      "https://productinformation.sandvik.coromant.com/s3/documents/pictures/pict-3d-view/ext-preview/44372813__400X40072Dpi.png",
-    description:
-      "Vysoce výkonný monolitní karbidový vrták Sandvik CoroDrill® 860-MM určený pro přesné vrtání ocelí 42–55 HRC. Varianta M2BM s vnitřním chlazením.",
-    parameters: {
-      cuttingDiameter: "10.5 mm",
-      protrudingLength: "73 mm",
-      usableLength: "56 mm",
-      overallLength: "118 mm",
-      chipFluteLength: "71 mm",
-      pointLength: "1.911 mm",
-      pointAngle: "140°",
-      fluteCount: 2,
-      helixAngle: "30°",
-      shankDiameter: "12 mm h6",
-      shankLength: "45 mm",
-      material: "HW – tvrdokov (karbid)",
-      grade: "M2BM",
-      coolant: "Vnitřní chlazení – axiální vstup / výstup",
-      maxRPM: "1971 ot/min",
-      weight: "0.119 kg",
-    },
+    gtin: "–",
+    diameter: "10.5 mm",
+    image: "https://productinformation.sandvik.coromant.com/s3/documents/pictures/pict-3d-view/ext-preview/44372813__400X40072Dpi.png",
+    description: "Vysoce výkonný monolitní vrták Sandvik 860.1 pro přesné vrtání otvorů.",
   },
-
-  // -------------------------------------------------------------------
-  // DALŠÍ NÁSTROJE MŮŽEME PŘIDAT SEM
-  // -------------------------------------------------------------------
+  {
+    id: 2,
+    name: "860.1-1050-056A1-MM",
+    name_display: "Sandvik Coromant 860.1-1050-056A1-MM – varianta B",
+    type: "Vrták",
+    manufacturer: "Sandvik Coromant",
+    gtin: "–",
+    diameter: "10.5 mm",
+    image: "https://common-secoresources.azureedge.net/pictures/core/Content/ProductImages/As_Delivered_Image/2017-04-10-140905_20175009.jpg?v=2017-04-10%2014:09:05Z",
+    description: "Alternativní varianta vrtáku z PDF datasheetu.",
+  },
+  {
+    id: 3,
+    name: "SD205A-1050-056-12R1-P",
+    name_display: "SD205A-1050-056-12R1-P",
+    type: "Vrták",
+    manufacturer: "Seco",
+    gtin: "–",
+    diameter: "10.5 mm",
+    image: "https://common-secoresources.azureedge.net/pictures/core/Content/ProductImages/As_Delivered_Image/2017-04-10-140905_20175009.jpg?v=2017-04-10%2014:09:05Z",
+    description: "Seco vrták SD205A pro hluboké vrtání s chladicími kanály.",
+  }
 ];
