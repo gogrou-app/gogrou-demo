83239c3ae29ed794f847b5c9eae4b7fa3f309855
gogrou-app <gogrou@gogrou.com>
2025-12-26 13:25:58 +0100

    Update locations.js

--- app/gss/data/locations.js
+++ app/gss/data/locations.js
@@ -1,7 +1,32 @@
-const locations = {
-  MAIN: "Hlavní sklad",
-  MACHINE: "Ve výrobě",
-  SERVICE: "Servis"
-};
+// GSS – dostupné lokace pro DM pohyby
+// zákazník si je později bude upravovat
+
+const locations = [
+  {
+    id: "warehouse:MAIN",
+    label: "Hlavní sklad",
+    type: "warehouse"
+  },
+  {
+    id: "warehouse:STM01",
+    label: "STM01 – výdejna",
+    type: "warehouse"
+  },
+  {
+    id: "machine:CNC_MAZAK_01",
+    label: "CNC Mazak 1",
+    type: "machine"
+  },
+  {
+    id: "machine:CNC_MAZAK_02",
+    label: "CNC Mazak 2",
+    type: "machine"
+  },
+  {
+    id: "service:BRUSIRNA",
+    label: "Brusírna / servis",
+    type: "service"
+  }
+];
 
 export default locations;
