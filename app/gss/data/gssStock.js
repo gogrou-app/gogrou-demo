6cc6f92b1122deaf4050e34ae78de049831cfb8f
gogrou-app <gogrou@gogrou.com>
2025-12-26 11:26:47 +0100

    Create gssStock.js

---
+++ app/gss/data/gssStock.js
@@ -0,0 +1,21 @@
+const gssStock = [
+  {
+    gss_stock_id: "STOCK-001",
+    gpc_id: "T01",
+    tracking_mode: "dm",
+    items: [
+      {
+        dm_code: "DM-001",
+        status: "in_stock",
+        resharpen_count: 0
+      },
+      {
+        dm_code: "DM-002",
+        status: "in_stock",
+        resharpen_count: 1
+      }
+    ]
+  }
+];
+
+export default gssStock;
