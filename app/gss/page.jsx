5c417897b7a71665c1859ff1bc089820b3a3d38b
gogrou-app <gogrou@gogrou.com>
2025-12-27 21:03:55 +0100

    Update page.jsx

--- app/gss/page.jsx
+++ app/gss/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect } from "react";
-import ContextHeader from "../components/ContextHeader";
 import { useAppContext } from "../context/AppContext";
 import { gssData } from "./data/gssStore";
 
