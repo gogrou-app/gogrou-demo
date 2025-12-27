d3b5dcec4037425234d00941347bcbca7808dd54
gogrou-app <gogrou@gogrou.com>
2025-12-27 19:39:46 +0100

    Update page.js

--- app/ss/page.js
+++ app/ss/page.js
@@ -1,18 +1,24 @@
-export default function Page() {
+"use client";
+
+import { useEffect } from "react";
+import ContextHeader from "../components/ContextHeader";
+import { useAppContext } from "../context/AppContext";
+
+export default function SmartSplitPage() {
+  const { setModule } = useAppContext();
+
+  useEffect(() => {
+    setModule("SmartSplit");
+  }, [setModule]);
+
   return (
-    <div style={{ padding: "40px" }}>
-      <h1>SmartSplit – dynamické akce</h1>
+    <div style={{ padding: 30, color: "white" }}>
+      <ContextHeader />
 
-      <p style={{ fontSize: "20px", opacity: 0.8, marginTop: "10px" }}>
-        Zde bude ukázka toho, jak fungují SmartSplit akce.
+      <h1>SmartSplit</h1>
+      <p style={{ opacity: 0.7 }}>
+        Zatím placeholder – později akce, ceny, množství, rozpad objednávek…
       </p>
-
-      <ul style={{ marginTop: "20px", fontSize: "18px", lineHeight: "1.6" }}>
-        <li>Hromadné nákupy a množstevní slevy</li>
-        <li>Výrobci mohou nastavovat procenta a pravidla</li>
-        <li>Automatické plánování odběrů</li>
-        <li>Dynamické promo akce pro zákazníky</li>
-      </ul>
     </div>
   );
 }
