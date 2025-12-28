e0489f613e3b6780a467d8334def1d057a0f2b74
M-technologies s.r.o. <petrmac@M-technologies-MacBook-Pro24.local>
2025-12-28 16:20:17 +0100

    fix: stabilize gss page with AppContext

---
+++ app/gss/page.js
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect } from "react";
+import ContextHeader from "../components/ContextHeader";
+import { useAppContext } from "../context/AppContext";
+
+export const dynamic = "force-dynamic";
+
+export default function GssPage() {
+  const { setModule } = useAppContext();
+
+  useEffect(() => {
+    setModule("GSS");
+  }, [setModule]);
+
+  return (
+    <div style={{ padding: 30, color: "white" }}>
+      <ContextHeader />
+
+      <h1>GSS – Gogrou Storage System</h1>
+      <p style={{ opacity: 0.7 }}>
+        Tady bude sklad, min/max, pohyby, DM kódy, servis, objednávky…
+      </p>
+    </div>
+  );
+}
